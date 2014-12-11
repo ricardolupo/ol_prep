95b258219a4379abef3a485c12769fdf741f7135
ricardolupo <rlupo@getchef.com>
2014-12-11 09:43:01 -0500

    refactored default.rb for all CHEF DSL

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,24 +6,28 @@
 # 
 # We will install a local yum repo and the chef-client
 
-file "/etc/yum.repos.d/public-yum-ol6.repo" do
+orig_repo = "/etc/yum.repos.d/public-yum-ol6.repo"
+new_repo = "/etc/yum.repos.d/public-yum-ol6.repo"
+source_repo = "http://ocipd-file01.us.oracle.com/software/repo/OracleLinux/gde-ol6.repo"
+
+
+
+file "#{orig_repo}" do
 	action :delete
 end
 
 execute "clean-yum-cache" do
 	command "yum clean all"
+  action :nothing
 end
 
-remote_file "/etc/yum/repos.d/gde-ol6.repo" do
-	source "http://ocipd-file01.us.oracle.com/software/repo/OracleLinux/gde-ol6.repo"
+remote_file "#{new_repo}" do
+	source "#{source_repo}"
  	action :create_if_missing
  	notifies :run, "execute[clean-yum-cache]", :immediately
+ 	not_if { ::File.exists?("#{new_repo}") }
 end
 
 package "screen" do 
 	action :install
-end
-
-service "screen" do
-	action [:enable, :start]
 end
